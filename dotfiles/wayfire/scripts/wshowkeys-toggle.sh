6a9d69f6c71c2ce0e9a97831e8b5836c697181f0
Tim Sutton <tim@kartoza.com>
2025-12-25 11:03:20 +0000

    wshowkeys setup

--- dotfiles/wayfire/scripts/wshowkeys-toggle.sh
+++ dotfiles/wayfire/scripts/wshowkeys-toggle.sh
@@ -7,15 +7,16 @@ PIDFILE="$XDG_RUNTIME_DIR/wshowkeys.pid"
 
 # Check if wshowkeys is running
 if [ -f "$PIDFILE" ] && kill -0 "$(cat "$PIDFILE")" 2>/dev/null; then
-    # wshowkeys is running, kill it
-    kill "$(cat "$PIDFILE")"
-    rm -f "$PIDFILE"
-    notify-send "wshowkeys" "Key display disabled" --icon=input-keyboard
-    echo "disabled"
+  # wshowkeys is running, kill it
+  kill "$(cat "$PIDFILE")"
+  rm -f "$PIDFILE"
+  notify-send "wshowkeys" "Key display disabled" --icon=input-keyboard
+  echo "disabled"
 else
-    # wshowkeys is not running, start it
-    wshowkeys -a bottom-right -t 2 -F "DejaVu Sans Mono:size=14" &
-    echo $! > "$PIDFILE"
-    notify-send "wshowkeys" "Key display enabled" --icon=input-keyboard
-    echo "enabled"
+  # wshowkeys is not running, start it
+  wshowkeys -F "Nurito 90" -t 3 -m 90 -a top -a right -b "#51585D" -f "#ffffff" -s "#E4AE52"
+  echo $! >"$PIDFILE"
+  notify-send "wshowkeys" "Key display enabled" --icon=input-keyboard
+  echo "enabled"
 fi
+
